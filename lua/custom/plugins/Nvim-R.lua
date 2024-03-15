fb5afff3a0c102760dec192d942965f05b493fe6
Julian Dicken <julian@dicken.de>
2024-03-15 16:39:54 +0100

    +mini.animate and some other things i think

---
+++ lua/custom/plugins/Nvim-R.lua
@@ -0,0 +1,10 @@
+return {
+    "R-nvim/R.nvim",
+    lazy = false,
+    opts = {
+        active_window_warn = false,
+        auto_start = "always",
+        rnowebchunk = false,
+        assign_map = "_",
+    }
+}
