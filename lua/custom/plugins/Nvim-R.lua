912d755505bd8a9662ad3327d8ea13b95430af0d
Julian Dicken <julian@dicken.de>
2024-03-19 15:41:32 +0100

    +nio -nvim-r branch

--- lua/custom/plugins/Nvim-R.lua
+++ lua/custom/plugins/Nvim-R.lua
@@ -1,10 +1,10 @@
 return {
-    "R-nvim/R.nvim",
-    lazy = false,
-    opts = {
-        active_window_warn = false,
-        auto_start = "always",
-        rnowebchunk = false,
-        assign_map = "_",
-    }
+  'R-nvim/R.nvim',
+  lazy = false,
+  opts = {
+    active_window_warn = false,
+    auto_start = 'always',
+    rnowebchunk = false,
+    assign_map = '_',
+  },
 }
